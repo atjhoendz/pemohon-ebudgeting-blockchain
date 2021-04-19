ccba126c5c4114555a0fe480282d070ff472a1c4
atjhoendz <achunarmando@gmail.com>
2021-04-19 11:24:46 +0700

    feat(auth): implement API logout

--- src/containers/TheHeaderDropdownAccnt.vue
+++ src/containers/TheHeaderDropdownAccnt.vue
@@ -1,26 +1,45 @@
 <template>
-  <CDropdown inNav class="c-header-nav-items" placement="bottom">
-    <template #toggler-content>
-      <span class="h4 d-md-down-none" style="color: #3c4b64">{{
-        $store.state.user.currentUser.username || 'username'
-      }}</span>
-      <span class="h5 d-lg-none" style="color: #3c4b64">{{
-        $store.state.user.currentUser.username || 'username'
-      }}</span>
-    </template>
-    <CDropdownItem to="/login">
-      <CIcon name="cil-lock-locked" /> Logout
-    </CDropdownItem>
-  </CDropdown>
+  <div>
+    <CDropdown inNav class="c-header-nav-items" placement="bottom">
+      <template #toggler-content>
+        <span class="h4 d-md-down-none" style="color: #3c4b64">{{
+          $store.state.user.currentUser.username || 'username'
+        }}</span>
+        <span class="h5 d-lg-none" style="color: #3c4b64">{{
+          $store.state.user.currentUser.username || 'username'
+        }}</span>
+      </template>
+      <CDropdownItem @click="makeLogout()">
+        <CIcon name="cil-lock-locked" /> Logout
+      </CDropdownItem>
+    </CDropdown>
+    <toast-msg :listToasts="listToasts" />
+  </div>
 </template>
 
 <script>
+import ToastMsg from '../components/ToastMsg'
+import { AuthService } from '../services/auth.service'
+
 export default {
   name: 'TheHeaderDropdownAccnt',
+  components: { ToastMsg },
   data() {
     return {
-      itemsCount: 42,
+      listToasts: [],
     }
+  },
+  methods: {
+    async makeLogout() {
+      try {
+        await AuthService.makeLogout()
+      } catch (err) {
+        this.listToasts.push({
+          message: 'Terjadi masalah. Logout tidak berhasil.',
+          color: 'danger',
+        })
+      }
+    },
   },
 }
 </script>
