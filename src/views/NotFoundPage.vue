07a38a7b7fb06dba4a6febc22edecb06ca968760
atjhoendz <achunarmando@gmail.com>
2021-04-23 14:17:18 +0700

    feat(route): add notfoundpage handler

---
+++ src/views/NotFoundPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="d-flex flex-column justify-content-center align-items-center"
+    style="height: 100vh;"
+  >
+    <h2>404 page not found.</h2>
+    <CButton class="mt-3 text-primary" to="/">Back to home</CButton>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
